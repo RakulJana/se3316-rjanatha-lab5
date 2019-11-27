c4d738a3f850c97906c3af50208f3da3ee49c79b
Rakul Janatharan <rjanatha@uwo.ca>
2019-11-27 09:31:59 -0500

    Adding an initial route to test postman
    testing postman

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-
+const users = [] // we are using a local variable for now
 
 
 
@@ -24,6 +24,24 @@ var router = express.Router(); // instance of express.router
 //test route
 
 // more routes
+router.route('/users') // routes to users
+    .get(function(req, res){ // gets all registered users
+        res.json(users); // response is the json object of all users
+    })
+
+    .post(function(req,res){
+        // must create user, hash password and save it
+        // in the url i will be passing a username and password to the variable
+        
+    
+    });
+
 
 
 // register the route
+app.use('/', router)
+
+
+// listen
+app.listen(port);
+console.log(port);
