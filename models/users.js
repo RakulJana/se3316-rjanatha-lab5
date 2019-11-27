fc6b85b65350820f1f2a094a1a15c3546363e585
Rakul Janatharan <rjanatha@uwo.ca>
2019-11-27 09:40:17 -0500

    First User Model
    added a user model

---
+++ models/users.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose'); // to use mongo
+var Schema = mongoose.Schema;
+// basic schema for now, will add other auth levels later
+var UserSchema = new Schema({
+    name: String,
+    password: String
+});
+// export the user schema
+module.exports = mongoose.model('User', UserSchema);
